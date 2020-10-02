936cdc43e14f73a8b6a64708ab50dd31b6cb0eca
muji <muji@tmpfs.org>
2020-10-02 10:40:52 +0800

    Expect BUILD_SOURCE env variable.
    Use path.join() so it should work cross-platform.

--- tool/tailwind/build.js
+++ tool/tailwind/build.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const execSync = require('child_process').execSync;
 const env = process.env.NODE_ENV;
+const source = process.env.BUILD_SOURCE;
 const target = process.env.BUILD_TARGET;
 
 function fatal(message) {
@@ -7,16 +9,12 @@ function fatal(message) {
   process.exit(1);
 }
 
-if (!env) {
-  fatal("NODE_ENV must be set");
-}
+if (!env) { fatal("NODE_ENV must be set"); }
+if (!source) { fatal("BUILD_SOURCE must be set"); }
+if (!target) { fatal("BUILD_TARGET must be set"); }
 
-if (!target) {
-  fatal("BUILD_TARGET must be set");
-}
-
-const source = "site/assets/css/style.css";
-const output = `${target}/assets/css/style.css`;
+const source = path.join(source, "assets", "css", "style.css");
+const target = path.join(target, "assets", "css", "style.css");
 const cmd = `npx postcss ${source} -o ${output}`;
 
 execSync(cmd, {stdio: 'inherit'});
