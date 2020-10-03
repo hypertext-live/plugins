3bd5a95286fc19cee8074851eec761ffdaa33c03
muji <muji@tmpfs.org>
2020-10-03 11:50:31 +0800

    Tidy build script.

--- tool/tailwind/build.js
+++ tool/tailwind/build.js
@@ -17,6 +17,7 @@ if (!HOOK) { fatal("BUILD_HOOK must be set"); }
 const source = path.join(SOURCE, "assets", "css", "style.css");
 const output = path.join(TARGET, "assets", "css", "style.css");
 const config = path.join(HOOK, "postcss.config.js");
-const cmd = `./node_modules/postcss-cli/bin/postcss ${source} -o ${output} --config ${config}`;
+const postcss = "./node_modules/postcss-cli/bin/postcss";
+const cmd = `${postcss} ${source} -o ${output} --config ${config}`;
 
 execSync(cmd, {stdio: 'inherit'});
