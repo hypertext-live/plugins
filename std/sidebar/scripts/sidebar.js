badbbe8e328c7df182f7d70118173eb255674b2a
muji <muji@tmpfs.org>
2020-12-02 13:37:49 +0800

    Simple script to remember sidebar open state.

---
+++ std/sidebar/scripts/sidebar.js
@@ -0,0 +1,10 @@
+(function sidebar() {
+  let key = "sidebar-open";
+  let el = document.getElementById("sidebar");
+  if (el && sessionStorage) {
+    el.checked = sessionStorage.getItem(key);
+    el.addEventListener("change", (e) => {
+      sessionStorage.setItem(key, e.currentTarget.checked);
+    });
+  }
+})();
